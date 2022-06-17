12dec080aba3d6ebe6a70ef01fcebf990123b9fb
dimaNuts <nuts272@mail.ru>
2022-06-17 20:09:32 +0300

    Задание: ждем нужный текст

--- stepic_248.py
+++ stepic_248.py
@@ -1,4 +1,4 @@
-import math
+from math import sin, log
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -9,20 +9,17 @@ try:
 
     browser = webdriver.Chrome()
     browser.get("http://suninjuly.github.io/explicit_wait2.html")
-    # говорим Selenium проверять в течение 5 секунд, пока кнопка не станет кликабельной
     price = WebDriverWait(browser, 12).until(
         EC.text_to_be_present_in_element((By.CSS_SELECTOR, "#price"), "100")
     )
     button = browser.find_element(By.CSS_SELECTOR, "#book")
     button.click()
-    # находим переменную "x" на странице сайта
     x_el = browser.find_element(By.CSS_SELECTOR, "#input_value")
     x = x_el.text
-    # создаем функцию для нахождения значения "х"
 
     def calc(n):
         """ Вычисляет значение выражения переменной 'х' """
-        return str(math.log(abs(12 * math.sin(int(n)))))
+        return str(log(abs(12 * sin(int(n)))))
 
 
     # вычисляем значение функции
